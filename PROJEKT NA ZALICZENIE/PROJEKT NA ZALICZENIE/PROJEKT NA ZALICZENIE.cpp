9d61d9cfb3659e42d7e6214b24b8f1b3b8446597
Ragatais <ragahuehue@gmail.com>
2022-01-16 18:56:10 +0100

    dodane komunikaty

--- PROJEKT NA ZALICZENIE/PROJEKT NA ZALICZENIE/PROJEKT NA ZALICZENIE.cpp
+++ PROJEKT NA ZALICZENIE/PROJEKT NA ZALICZENIE/PROJEKT NA ZALICZENIE.cpp
@@ -5,23 +5,23 @@
 
 void menu(bool sortType, bool sortOrder)
 {
-    std::cout << "\n1. Wczytaj. \nTyp sortowania: ";
+    std::cout << "\n1. Wczytaj.\nTyp sortowania: ";
     if (sortType)
     {
-        std::cout << "Babelkowe \n";
+        std::cout << "Babelkowe\n";
     }
     else
     {
-        std::cout << "Szybkie \n";
+        std::cout << "Szybkie\n";
     }
     std::cout << "Kolejnosc sortowania: ";
     if (sortOrder)
     {
-        std::cout << "A-Z \n";
+        std::cout << "A-Z\n";
     }
     else
     {
-        std::cout << "Z-A \n";
+        std::cout << "Z-A\n";
     }
     std::cout << "2. Zmien tryb sortowania. \n3. Zmien kolejnosc sortowania. \n4. Sortuj. \n";
     std::cout << "5. Wyswietl najdluzszy i najkrotszy wyraz. \n6. Dodaj wyraz. \n";
@@ -29,31 +29,34 @@ void menu(bool sortType, bool sortOrder)
 }
 void readFromFile(std::vector<std::string>* dictionary)
 {
-    dictionary->clear();
-    std::fstream file;
-    std::string pathToFile, line;
+    dictionary->clear();                                        //czysczenie tablicy na wypadek, gdyby byly w niej juz zapisane jakies dane
+    std::fstream file;                                          //strumien danych z pliku
+    std::string pathToFile, line;                               
 
     std::cout << "Podaj sciezke do pliku: ";
     std::cin >> pathToFile;
 
-    file.open(pathToFile,std::ios::in);
-    int spacePosition;
+    file.open(pathToFile,std::ios::in);                         //otwieranie pliku w trtybie tylko do odczytu
+    int spacePosition;                                          //oddzielanie numeracji od wyrazow
     std::getline(file, line);
     do
     {
         std::getline(file, line);
         spacePosition = line.find(" ");
         if(spacePosition!=-1)
-            line = line.substr(spacePosition+1);
-        dictionary->push_back(line);
+            line = line.substr(spacePosition+1);                
+        dictionary->push_back(line);                            //dodawanie wyrazow na koniec tablicy
     } while (line != "");
 
-    dictionary->pop_back();
+    dictionary->pop_back();                                     //(*dictionary).pop_back();
     file.close();
+
+    std::cout << "Plik zostal wczytany.\n";
 }
 void bubbleSort(std::vector<std::string>* dictionary)
 {
-    bool flag = true;
+    bool flag = true;  
+    std::string tmp;                                         //czy zostala dokonana jakas zmiana
     while (flag)
     {
         flag = false;
@@ -62,8 +65,7 @@ void bubbleSort(std::vector<std::string>* dictionary)
             if ((*dictionary)[i] > (*dictionary)[i + 1])
             {
                 flag = true;
-                std::string tmp;
-                
+               
                 tmp = (*dictionary)[i];
                 (*dictionary)[i] = (*dictionary)[i + 1];
                 (*dictionary)[i + 1] = tmp;
@@ -75,6 +77,7 @@ int part(std::vector<std::string>* dictionary, int start, int end)
 {
     std::string x = (*dictionary)[(start + end) / 2];
     int i = start, j = end;
+    std::string tmp;
     while (1)
     {
         while ((*dictionary)[j] > x)
@@ -87,8 +90,6 @@ int part(std::vector<std::string>* dictionary, int start, int end)
         }
         if (i < j)
         {
-            std::string tmp;
-
             tmp = (*dictionary)[i];
             (*dictionary)[i] = (*dictionary)[j];
             (*dictionary)[j] = tmp;
@@ -110,7 +111,6 @@ void quickSort(std::vector<std::string>* dictionary, int start, int end)
         middle = part(dictionary, start, end);
         quickSort(dictionary, start, middle);
         quickSort(dictionary, middle + 1, end);
-
     }
 }
 void reverse(std::vector<std::string>* dictionary)
@@ -151,6 +151,8 @@ void add(std::vector<std::string>* dictionary)
     std::cout << "Podaj wyraz, ktory chcesz dodac: ";
     std::cin >> word;
     dictionary->push_back(word);
+
+    std::cout << "Wyraz " << word << " zostal dodany do listy.\n";
 }
 void remove(std::vector<std::string>* dictionary)
 {
@@ -164,6 +166,7 @@ void remove(std::vector<std::string>* dictionary)
         {
             dictionary->erase(i);
             flag = false;
+            std::cout << "Wyraz " << word << " zostal usuniety z listy.\n";
             break;
         }
         else
@@ -178,32 +181,31 @@ void remove(std::vector<std::string>* dictionary)
 }
 void display(std::vector<std::string> dictionary)
 {
-    for (auto i = dictionary.begin(); i != dictionary.end(); i++)
+    for (int i = 0; i < dictionary.size(); i++)
     {
-        std::cout << *i << std::endl;
+        std::cout << dictionary[i] << std::endl;
     }
 }
 void save(std::vector<std::string> dictionary)
 {
     std::fstream file;
     std::string pathToFile;
-    int iterator = 1;
-    std::cout << "Podaj sciezke do pliku: ";
+    std::cout << "Podaj sciezke do zapisu: ";
     std::cin >> pathToFile;
 
-    file.open(pathToFile,std::ios::out);
+    file.open(pathToFile,std::ios::out);                //jesli plik nie istnieje to zostaje utworzony
     file << "l.p.  word \n";
-    for (auto i = dictionary.begin(); i < dictionary.end(); i++)
-    {
-        
-        file<< iterator << " " << *i << std::endl;
-        iterator++;
+    for (int i = 0 ; i < dictionary.size(); i++)
+    { 
+        file << i+1 << ". " << dictionary[i] << std::endl;
     }
+
+    std::cout << "Plik zostal zapisany.\n";
 }
 
 int main()
 {
-    std::vector<std::string> dictionary;
+    std::vector<std::string> dictionary;            //tablica bez deklarowanej wielkosci, typ string
     bool sortType = true, sortOrder = true;
     int choice = 0;
     
@@ -218,15 +220,23 @@ int main()
             break;
         case 2:
             sortType = !sortType;
+            std::cout << "Typ sortowania zostal zmieniony.\n";
             break;
         case 3:
             sortOrder = !sortOrder;
+            std::cout << "Kolejnosc sortowania zostala zmieniona.\n";
             break;
         case 4:
             if (sortType)
+            {
                 bubbleSort(&dictionary);
+                std::cout << "Wyrazy zostaly posortowane.\n";
+            }
             else
-                quickSort(&dictionary, 0, dictionary.size()-1);
+            {
+                quickSort(&dictionary, 0, dictionary.size() - 1);
+                std::cout << "Wyrazy zostaly posortowane.\n";
+            }
             if (!sortOrder)
                 reverse(&dictionary);
             break;
